50e9e4146878bed15aa87358f9a99004df4dd4e6
Ranit-dev2004 <saharanit16@gmail.com>
2025-08-22 17:56:15 +0530

    fix: Correct case of Hero4 image import

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import Logo from './Group.png';
 import Hero1 from './Hero1.png';
 import Hero2 from './Hero2.png';
 import Hero3 from './Hero3.png';
-import Hero4 from './Hero4.png';
+import Hero4 from './hero4.png';
 import Hero5 from './Hero5.jpg'
 
 export const assets = {
